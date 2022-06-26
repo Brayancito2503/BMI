537bc90db9250ce58e6d1eb45bbabdbf3f415c38
Richard <richardgomezbriones@gmail.com>
2022-06-26 14:27:39 -0600

    se agregaron spinners y se obtuvieron los indices de los mismos, solo falta las validaciones

--- app/src/main/java/com/example/pesoapp/View/Fragment_Calculadora.kt
+++ app/src/main/java/com/example/pesoapp/View/Fragment_Calculadora.kt
@@ -1,5 +1,4 @@
 package com.example.pesoapp.View
-
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,37 +7,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.get
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pesoapp.R
 import com.example.pesoapp.databinding.FragmentCalculadoraBinding
-import com.github.anastr.speedviewlib.SpeedView
-import com.github.anastr.speedviewlib.components.Section
 import com.github.anastr.speedviewlib.components.Style
-import java.lang.Boolean.TRUE
 import kotlin.math.pow
+import kotlin.properties.Delegates
 
 
-class Fragment_Calculadora : Fragment(), AdapterView.OnItemClickListener {
+class Fragment_Calculadora : Fragment(){
 
     //Declaracion de variables
     private var fbinding: FragmentCalculadoraBinding? = null
     private val binding get() = fbinding!!
+    private var itemaltura by Delegates.notNull<Int>()
+    private var itempeso by Delegates.notNull<Int>()
 
     var f1 = 0f
     var f2 = 0f
 
     var item:String = ""
-    var item2:String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         fbinding = FragmentCalculadoraBinding.inflate(layoutInflater)
         val view = binding.root
@@ -52,8 +45,6 @@ class Fragment_Calculadora : Fragment(), AdapterView.OnItemClickListener {
                 medidasEstatura
             )
         }
-
-
         val adapterPeso = context?.let {
             ArrayAdapter(
                 it,
@@ -61,20 +52,41 @@ class Fragment_Calculadora : Fragment(), AdapterView.OnItemClickListener {
                 medidasPeso
             )
         }
-//
-        with(binding.autoCompleteEstaturaMedida){
-            setAdapter(adapterEstatura)
-            onItemClickListener = this@Fragment_Calculadora
-//            onItemSelectedListener = this@Fragment_Calculadora
+        //Spinners
+        val SpinnerAltura= binding.spinner1altura
+        val SpinnerPeso= binding.spinner2peso
+
+        //Spinner para Estatura
+        if(SpinnerAltura!=null){
+
+            SpinnerAltura.adapter=adapterEstatura
+
+            SpinnerAltura.onItemSelectedListener=object:AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    itemaltura=p2 // Variable que captura el indice del spinner de altura
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
         }
-        with(binding.autoCompletePesoMedida){
-            setAdapter(adapterPeso)
-            onItemClickListener = this@Fragment_Calculadora
-//            onItemSelectedListener = this@Fragment_Calculadora
+        //Spinner para peso
+        if(SpinnerPeso!=null){
+
+            SpinnerPeso.adapter=adapterPeso
+
+            SpinnerPeso.onItemSelectedListener=object:AdapterView.OnItemSelectedListener{
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    itempeso=p2 // Variable que captura el indice del spinner de peso
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
         }
 
-
-//        val speedometer = findViewById<SpeedView>(R.id.speedometer)
 
         //secciones de la grafica
         val speedometer = binding.speedometer
@@ -114,48 +126,11 @@ class Fragment_Calculadora : Fragment(), AdapterView.OnItemClickListener {
             var resultado = peso / (altura / 100).pow(2)
             var pi =((((altura - 152.4) / 2.54) * f1) + f2)
 
-//            if(item == "cm" && item2 == "lb"){
-//
-//                resultado = ((peso/2.2) / (altura / 100).pow(2)).toFloat()
-//                pi = ((((altura - 152.4) / 2.54) * f1) + f2)
-//                Toast.makeText(context, "entro al primero < Pi $pi > <R $resultado >", Toast.LENGTH_SHORT).show()
-//                if (resultado < 18) {
-//                    lblInc.text = "Debajo de lo normal $resultado"
-//                } else if (resultado >= 18.1 && resultado <= 24.9) {
-//                    lblInc.text = "Peso Normal $resultado"
-//                } else if (resultado >= 25 && resultado <= 29.9) {
-//                    lblInc.text = "Sobre Peso $resultado"
-//                } else if (resultado >= 30 && resultado <= 34.9) {
-//                    lblInc.text = "Obesidad tipo I $resultado"
-//                } else if (resultado > 35) {
-//                    lblInc.text = "Obesidad tipo II $resultado"
-//                }
-//                lblPi.text = "$pi"
-//                Toast.makeText(context,"$peso",Toast.LENGTH_SHORT).show()
-//                speedometer.speedTo(resultado, 4000)
-//            }else if(item == "m" && item2 == "lb"){
-//
-//
-//                resultado = ((peso/2.2)/((altura).pow(2))).toFloat()
-//                pi = (((((altura * 100 ) - 152.4) / 2.54) * f1) + f2)
-//
-//                Toast.makeText(context, "entro al segungo < Pi $pi > <R $resultado >", Toast.LENGTH_SHORT).show()
-//                if (resultado < 18) {
-//                    lblInc.text = "Debajo de lo normal $resultado"
-//                } else if (resultado >= 18.1 && resultado <= 24.9) {
-//                    lblInc.text = "Peso Normal $resultado"
-//                } else if (resultado >= 25 && resultado <= 29.9) {
-//                    lblInc.text = "Sobre Peso $resultado"
-//                } else if (resultado >= 30 && resultado <= 34.9) {
-//                    lblInc.text = "Obesidad tipo I $resultado"
-//                } else if (resultado > 35) {
-//                    lblInc.text = "Obesidad tipo II $resultado"
-//                }
-//                lblPi.text = "$pi"
-//                Toast.makeText(context,"$peso",Toast.LENGTH_SHORT).show()
-//                speedometer.speedTo(resultado, 4000)
-//            }
-//
+            //Imprime los valores de indice de cada spinner
+            Toast.makeText(context," Spinner altura indice es: $itemaltura",Toast.LENGTH_SHORT).show()
+            Toast.makeText(context,"Spinner peso indice es: $itempeso",Toast.LENGTH_SHORT).show()
+
+
 
             if (resultado < 18) {
                 lblInc.text = "Debajo de lo normal $resultado"
@@ -174,46 +149,11 @@ class Fragment_Calculadora : Fragment(), AdapterView.OnItemClickListener {
 
         }
 
-
-//        val speedometer =  binding<SpeedView>(R.id.speedometer)
-
         return view
-
-    }
-
-
-//    val aux:String = item
-//    var aux2:String = item2
-
-
-    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
-        val hola = resources.getStringArray(R.array.Estatura)
-        item = hola.get(position)
-        val hola2 = resources.getStringArray(R.array.Peso)
-        item2 = hola2.get(position)
-
-//        Toast.makeText(context, aux, Toast.LENGTH_SHORT).show()
-        Toast.makeText(context, "item 1 $item y item 2 $item2", Toast.LENGTH_SHORT).show()
-
-
-        if(item == "cm" || item == "m"){
-            binding.alturaTxt.isEnabled = true
-            Toast.makeText(context,"es el item 1 $item", Toast.LENGTH_SHORT).show()
-        }
-        if(item2 == "lb" || item2 == "kg"){
-            binding.pesoTxt.isEnabled = true
-            Toast.makeText(context,"es el item 2 $item2", Toast.LENGTH_SHORT).show()
-        }
-//
-//        if(item == "cm" && item2 == "lb"){
-//            Toast.makeText(context, "si sirve item 1 <$item > item 2 <$item2> ", Toast.LENGTH_SHORT).show()
-//        }
-//
-
 
     }
 
 
 
 }
+

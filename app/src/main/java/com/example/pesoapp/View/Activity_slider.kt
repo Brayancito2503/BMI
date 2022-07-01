50e4fa5d325389e268607dd85f5a35b10a6a0cca
Cris432L <cristinajamilethlopezflores@hotmail.com>
2022-07-01 03:23:11 -0600

    Actualizacion de animaciones en intro slider

--- app/src/main/java/com/example/pesoapp/View/Activity_slider.kt
+++ app/src/main/java/com/example/pesoapp/View/Activity_slider.kt
@@ -1,17 +1,29 @@
 package com.example.pesoapp.View
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.example.pesoapp.R
 import com.example.pesoapp.View.adapter.ViewPagerAdapter
 import com.example.pesoapp.databinding.ActivitySliderBinding
 
 class Activity_slider : AppCompatActivity() {
-    lateinit var binding: ActivitySliderBinding
+    private lateinit var binding: ActivitySliderBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_slider)
+        binding = ActivitySliderBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+       binding.viewpager.adapter=ViewPagerAdapter(supportFragmentManager)
+
+        /*val handler = Handler()
+        handler.postDelayed({ // Do something after 5s = 5000ms
+            val intent = Intent(this,Activity_login::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)*/
 
     }
 }
--- app/src/main/java/com/example/pesoapp/View/Onboarding1Fragment.kt
+++ app/src/main/java/com/example/pesoapp/View/Onboarding1Fragment.kt
@@ -1,60 +1,53 @@
 package com.example.pesoapp.View
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.airbnb.lottie.LottieAnimationView
 import com.example.pesoapp.R
+import com.example.pesoapp.databinding.ActivitySliderBinding
+import com.example.pesoapp.databinding.FragmentCalculadoraBinding
+import com.example.pesoapp.databinding.FragmentOnboarding1Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Onboarding1Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Onboarding1Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var fbinding: FragmentOnboarding1Binding? = null
+    private val binding get() = fbinding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    }
+
+    private fun likeAnimation(imageView: LottieAnimationView,
+                              animation: Int,
+                              like: Boolean) : Boolean {
+
+        if (!like) {
+            imageView.setAnimation(animation)
+            imageView.playAnimation()
+            imageView.repeatCount=3
         }
+        return !like
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding1, container, false)
+        fbinding = FragmentOnboarding1Binding.inflate(layoutInflater)
+        val view = binding.root
+        var like = false
+
+        likeAnimation(binding.imglogo, R.raw.bmi, like)
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Onboarding1Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Onboarding1Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
 }

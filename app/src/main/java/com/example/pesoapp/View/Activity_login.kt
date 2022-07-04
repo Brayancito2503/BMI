0d9e6c59db0909e7ab466550e851637b67be0559
Richard <richardgomezbriones@gmail.com>
2022-07-03 19:09:59 -0600

    autologin

--- app/src/main/java/com/example/pesoapp/View/Activity_login.kt
+++ app/src/main/java/com/example/pesoapp/View/Activity_login.kt
@@ -7,84 +7,93 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.pesoapp.R
 import com.example.pesoapp.databinding.ActivityLoginBinding
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class Activity_login : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        //----------------------
-        val view = binding.root
-        setContentView(view)
-        val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-        binding.btnIniciar.setOnClickListener { v ->
-            if (validarUsuario().equals(true)) {
-                firebaseAuth.signInWithEmailAndPassword(
-                    binding.tvUsername.text.toString(),
-                    binding.password.text.toString()
-                )
-                    .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
-                        if (task.isSuccessful) {
-                            startActivity(Intent(this, Activity_menu::class.java))
-                            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-                            binding.tvUsername.setText("")
-                            binding.password.setText("")
-                        } else {
-                            Toast.makeText(
-                                this,
-                                "El usuario y clave no existen",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    })
+        setContentView(binding.root)
+        auth = Firebase.auth
+        firebaseAuth= FirebaseAuth.getInstance()
+        checkuser()
+        binding.btnIniciar.setOnClickListener(::onEntrarBtnClicked)
+
+
+
+    }
+
+    private fun checkuser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser != null){
+            startActivity(Intent(this@Activity_login,Activity_menu::class.java))
+            finish()
+        }
+    }
+    private fun onEntrarBtnClicked(view: View) {
+
+        val mEmail=binding.tvUsername.text.toString()
+        val mPassword=binding.password.text.toString()
+
+        when{
+            mEmail.isEmpty()|| mPassword.isEmpty()->
+            {
+
+                if(mEmail.isEmpty())
+                {
+                    binding.tvUsername.setError("Ingrese el campo requerido")
+                }
+                if (mPassword.isEmpty())
+                {
+                    binding.password.setError("Ingrese el campo requerido")
+                }
+                Toast.makeText(baseContext, "Correo o Contraseña incorrectos o incompletos",
+                    Toast.LENGTH_SHORT).show()
+            }
+            else->
+            {
+                SignIn(mEmail,mPassword)
             }
         }
     }
 
-    fun onClickMusicLista(view: View?) {
-        val intent = Intent(this, Activity_menu::class.java)
-        startActivity(intent)
-    }
-
-    fun onClickCrearCuenta(view: View?) {
-        val intent = Intent(
-            this,
-            Activity_crearcuenta::class.java
-        )
-        startActivity(intent)
-    }
-
-    //Validar que los campos no sean nulos
-    fun validarUsuario(): Boolean {
-        try {
-            var validaok = false
-            //-- El email es un valor requerido
-            if (binding.tvUsername.text?.length?.equals(0)!!) {
-                binding.tvUsername.requestFocus()
-                binding.tvUsername.setError("Debe ingresar su correo electrónico")
-                return validaok
+    private fun SignIn(email:String,password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d("TAG", "signInWithEmail:success")
+                    Toast.makeText(baseContext, "Bienvenido",
+                        Toast.LENGTH_SHORT).show()
+                    reload()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
             }
-            //-- La contraseña es un valor requerido
-            if (binding.password.text?.length?.equals(0)!!) {
-                binding.password.requestFocus()
-                binding.password.setError("Debe ingresar una contraseña")
-                return validaok
-            }
-            validaok = true
-            return validaok
-        } catch (e: Exception) {
-            e.message?.let { Log.e("Error en valida", it) };
-            return false
-        }
-    }
+    }//Fin Funcion SignIn
+
+    private fun reload(){
+        val intent = Intent(this,Activity_menu::class.java)
+        this.startActivity(intent)
+    }//Fin de la Funcion reload
+
+
 }
